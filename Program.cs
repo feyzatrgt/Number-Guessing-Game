83165afe595229780405fa08aa696cd9843c4d9c
feyzatrgt <feyza.turgut38@gmail.com>
2026-04-13 19:56:08 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿const int MinValue = 1;
+const int MaxValue = 100;
+
+Random random = new Random();
+int secretNumber = random.Next(MinValue, MaxValue);
+int guess = 0;
+int attemptCount = 0;
+
+Console.WriteLine($"Welcome to the number guessing game! I have picked a number between {MinValue} and {MaxValue}.");
+
+while (guess != secretNumber) {
+    Console.Write("Enter your guess: ");
+
+    if (!int.TryParse(Console.ReadLine(), out guess)) {
+        Console.WriteLine("Please enter a valid number.");
+        continue;
+    }
+
+    attemptCount++;
+
+    if (guess < MinValue || guess > MaxValue) {
+        Console.WriteLine($"Please enter a number between {MinValue} and {MaxValue}.");
+    } else if (guess < secretNumber) {
+        Console.WriteLine("The secret number is higher.");
+    } else if (guess > secretNumber) {
+        Console.WriteLine("The secret number is lower.");
+    } else {
+        Console.WriteLine($"Congratulations! You guessed it in {attemptCount} attempts.");
+    }
+}
